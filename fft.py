7a04da4d221f63492b812bb7516083ff5c9b6f12
Aklima <aklima@syr.edu>
2023-12-03 15:48:33 -0500

    Updated

--- fft.py
+++ fft.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Load the noisy image using PIL
-noisy_image = Image.open('showeryz.png').convert('L')  # Open and convert to grayscale
+noisy_image = Image.open('int.png').convert('L')  # Open and convert to grayscale
 
 # Convert PIL Image to NumPy array
 noisy_np = np.array(noisy_image)
