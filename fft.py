96ef1ef78a529e2dac0dbfed5c5269093dba9f07
Zae Moore <emoore06@syr.edu>
2023-12-03 19:15:33 -0500

    Activity text, new test image

--- fft.py
+++ fft.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Load the noisy image using PIL
-noisy_image = Image.open('int.png').convert('L')  # Open and convert to grayscale
+noisy_image = Image.open('test_image.png').convert('L')  # Open and convert to grayscale
 
 # Convert PIL Image to NumPy array
 noisy_np = np.array(noisy_image)
@@ -11,15 +11,16 @@ noisy_np = np.array(noisy_image)
 # Apply FFT to the image
 f = np.fft.fft2(noisy_np)
 fshift = np.fft.fftshift(f)
-magnitude_spectrum = 20 * np.log(np.abs(fshift))
+magnitude_spectrum = np.log(np.abs(fshift)) #Log scale for easier visibility of the noise
 
-# Create a Gaussian-shaped filter
+# Create a 2D Gaussian-shaped filter to remove the noise
+# We are assuming Gaussian shape for the noise
 rows, cols = noisy_np.shape
 crow, ccol = rows // 2, cols // 2
 x = np.arange(cols) - ccol
 y = np.arange(rows) - crow
 X, Y = np.meshgrid(x, y)
-sigma = 20  # Adjust sigma for the Gaussian filter
+sigma = 50  # You can adjust this sigma for the Gaussian filter to make the image more or less clear! Try it!
 gaussian_filter = np.exp(-(X**2 + Y**2) / (2 * (sigma**2)))
 
 # Apply the Gaussian filter in the frequency domain
